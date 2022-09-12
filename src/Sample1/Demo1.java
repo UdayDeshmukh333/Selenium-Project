4d7b7e3731908d987893342689573d55982e0581
dell <dell@DESKTOP-EAIEMRB>
2022-09-12 16:04:26 +0530

    [Uday]

---
+++ src/Sample1/Demo1.java
@@ -0,0 +1,17 @@
+package Sample1;
+
+public class Demo1 
+{
+public static void main(String[] args) 
+{
+System.out.println("Hi");
+
+System.out.println("Hello");
+
+System.out.println("hello from rahul");
+
+System.out.println("HI From Uday");
+
+
+}
+}
---
+++ src/Sample1/Demo2.java
@@ -0,0 +1,15 @@
+package Sample1;
+
+public class Demo2 
+{
+public static void main(String[] args)
+{
+System.out.println("Hey");
+System.out.println("ji");
+
+System.out.println("hello from rahul");
+
+
+System.out.println("HI UDay");
+}
+}
